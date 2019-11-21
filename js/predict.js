726f696753cfa26a4e88bb3f75b347326e89c050
qurihara <qurihara@unryu.org>
2019-11-20 19:11:30 -0800

    ok

--- js/predict.js
+++ js/predict.js
@@ -1,3 +1,4 @@
+eruda.init();
 let modelname = './sign_language_vgg16/';
 //let CLASSES = {0:'zero', 1:'one', 2:'two', 3:'three', 4:'four',5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
 
@@ -37,7 +38,8 @@ function success(data) {
 //-----------------------
 
 $("#start-button").click(function(){
-	loadModel() ;
+	loadModel();
+	loadDict();
 	startWebcam();
 });
 
